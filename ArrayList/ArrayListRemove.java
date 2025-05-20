a7caa6f6a809a2b1cf419d135e34fbd550971bd3
Nishmitha Shetty <nishmithas439@gmail.com>
2025-05-20 23:36:04 +0530

    Add files via upload

---
+++ ArrayList/ArrayListRemove.java
@@ -0,0 +1,21 @@
+/*Write a java program for getting different colors through ArrayList interface and remove the
+2nd element and color "Blue" from the ArrayList*/
+package ArrayList_Programs;
+import java.util.*;
+public class ArrayListRemove {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> ls = new ArrayList<>();
+		ls.add("Orange");
+		ls.add("geen");
+		ls.add("pink");
+		ls.add("red");
+		ls.add("Blue");
+		System.out.println(ls);
+		ls.remove(2);
+		System.out.println(ls);
+		ls.remove("Blue");
+		System.out.println(ls);
+	}
+
+}
